15ed07c71ef0f6441725c3f7c8e7da7fcb0907fa
yunha <jhy3990585@innogrid.com>
2020-09-08 16:17:48 +0900

    commit2

--- order/src/main/java/com/order/springboot/service/OrderService.java
+++ order/src/main/java/com/order/springboot/service/OrderService.java
@@ -4,12 +4,14 @@ import com.kitchen.springboot.dto.KitchenDto;
 import com.order.springboot.domain.Order;
 import com.order.springboot.dto.OrderDto;
 import com.order.springboot.repository.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class OrderService {
 
@@ -25,8 +27,9 @@ public class OrderService {
         String url = "http://localhost:8081/api/v1/order/kitchen/newOrder";
 
         KitchenDto newOrder = new KitchenDto(orderDto.getMenuName(), orderDto.getQuantity());
-        restTemplate.postForObject(url, newOrder, KitchenDto.class);
+        Long id = restTemplate.postForObject(url, newOrder, Long.class);
 
+        log.info("id",id);
         return orderRepository.save(orderDto.toEntity()).getId();
     }
 
